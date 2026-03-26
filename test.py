39886d5adcc0900fc9c006b9797d40468862d91e
Raghav Pathak <raghav.pathak@avantika.edu.in>
2026-03-26 07:45:19 +0000

    Executed

--- test.py
+++ test.py
@@ -5,6 +5,7 @@
 # T — the number of test cases. Then the test cases follow.
 # The first and only line of each test case contains one integer 
 # X — the amount of water client drank today.
+
 t = int(input())
 for i in range(t):
     x = int(input())
